3f118cd458ab624139c82bca1484b992cd8f0870
meetx00719 <imeetpatel2022@gmail.com>
2026-04-30 18:31:25 -0700

    please

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -14,7 +14,7 @@ import double3 from "./assets/double3.jpg";
 import jacuzziRoom from "./assets/jacuzzi.jpg";
 import jacuzzi2 from "./assets/jacuzzi2.jpg";
 import jacuzzi3 from "./assets/jacuzzi3.jpg";
-import dream1 from "./assets/dream1.jpg";
+import dream1 from "./assets/single.jpg";
 
 const galleryImages = [
   { image: hero1, title: "Dream Inn Exterior" },
