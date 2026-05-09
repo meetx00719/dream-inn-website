e3759fed9e41a82e2f4075bd55ec233cf9f43251
meetx00719 <imeetpatel2022@gmail.com>
2026-05-08 22:34:54 -0700

    soccer

--- src/App.jsx
+++ src/App.jsx
@@ -691,7 +691,7 @@ function App() {
           </div>
 
           <div className="bookingTrustStrip">
-            <span> Experience the energy of FIFA </span>
+            <span>⚽️ Experience the energy of FIFA </span>
             <span>✅ Book Now , Pay Later</span>
             <span>⭐ 1000+ Happy Guests</span>
           </div>
